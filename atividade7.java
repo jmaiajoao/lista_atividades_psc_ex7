5f3abfa6a254c180583a4d2ef6f007d7553911f2
jmaiajoao <128436278+jmaiajoao@users.noreply.github.com>
2023-04-08 22:16:04 -0300

    Add files via upload

---
+++ atividade7.java
@@ -0,0 +1,28 @@
+
+
+// programa de soluções computacionais.
+// Lista 2 ( atividade 7)
+//Aluno: João Victor Maia Leite
+//RA: 323124931
+
+
+
+import java.util.Scanner;
+
+public class atividade7 {
+    public static void main(String[] args) throws Exception {
+      
+
+Scanner input = new Scanner(System.in);
+
+System.out.println("Digite altura");
+double altura = input.nextDouble();
+
+double pesoideal = (72.7*altura) - 58 ; 
+
+
+System.out.println("Peso ideal"+ pesoideal);
+
+}
+
+}
